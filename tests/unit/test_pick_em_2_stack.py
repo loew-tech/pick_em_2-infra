84c2b6f62b4939987facbff7a4aed9883d219b80
loew-tech <loew.technology@gmail.com>
2026-08-04 09:27:25 -0600

    initial commit

---
+++ tests/unit/test_pick_em_2_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from pick_em_2.pick_em_2_stack import PickEm2Stack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in pick_em_2/pick_em_2_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = PickEm2Stack(app, "pick-em-2")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
